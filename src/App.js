930d006346a5d561fbe0232739ae7144a387da58
JZachrisson <jesper.zachrisson@gmail.com>
2021-03-09 12:43:16 +0100

    try again

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 const App = () => {
-    return <div className="App">Hej</div>;
+    return <div className="App">Hej!</div>;
 };
 
 export default App;
